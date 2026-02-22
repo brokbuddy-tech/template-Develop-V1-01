5c909d64c4897fbd131e78b3e690088ccf22774d
BrokBuddy <brokbuddy@gmail.com>
2026-02-22 08:04:03 +0000

    remove drop down (_for element <Button>_)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -28,10 +28,6 @@ export const NAV_LINKS: NavLink[] = [
   {
     href: '/commercial',
     label: 'Commercial',
-    children: [
-      { href: '/commercial/for-sale', label: 'For Sale' },
-      { href: '/commercial/for-rent', label: 'For Rent' },
-    ],
   },
   {
     href: '/off-plan',
